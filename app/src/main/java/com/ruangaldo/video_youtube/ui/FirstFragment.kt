8bc45844650f31be4ff47345f8ba0346aaab6245
Reinaldo Riant Kurnia Perdana <reinaldo.riant@kompas.com>
2021-04-07 15:47:12 +0700

    !First Fragment and Player Screen

--- app/src/main/java/com/ruangaldo/video_youtube/ui/FirstFragment.kt
+++ app/src/main/java/com/ruangaldo/video_youtube/ui/FirstFragment.kt
@@ -2,15 +2,23 @@ package com.ruangaldo.video_youtube.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.ruangaldo.video_youtube.PlayerScreen
+import com.ruangaldo.video_youtube.R
 import com.ruangaldo.video_youtube.base.BaseFragment
 import com.ruangaldo.video_youtube.databinding.FragmentFirstBinding
 
 class FirstFragment : BaseFragment<FragmentFirstBinding>() {
-
+    private var playScreenFragment = PlayerScreen.newInstance()
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentFirstBinding
         get() = FragmentFirstBinding::inflate
 
     override fun setup() {
+        binding.button.setOnClickListener {
+            childFragmentManager.beginTransaction()
+                .replace(R.id.play_screen_frame_layout, playScreenFragment)
+                .commitNow()
 
+        }
     }
+
 }
