034fde4346fc72ed3097cb0180fced4cd0fcf381
Alva Yonara Puramandya <alvayonara@outlook.com>
2021-04-08 14:51:18 +0700

    ! modify pass data to Player Screen using bundle

--- app/src/main/java/com/ruangaldo/video_youtube/ui/FirstFragment.kt
+++ app/src/main/java/com/ruangaldo/video_youtube/ui/FirstFragment.kt
@@ -1,5 +1,6 @@
 package com.ruangaldo.video_youtube.ui
 
+import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -7,19 +8,32 @@ import android.widget.Toast
 import com.ruangaldo.video_youtube.R
 import com.ruangaldo.video_youtube.base.BaseFragment
 import com.ruangaldo.video_youtube.databinding.FragmentFirstBinding
+import com.ruangaldo.video_youtube.ui.PlayerScreen.Companion.EXTRA_ARTICLE
+import com.ruangaldo.video_youtube.utils.DataDummy
+import com.ruangaldo.video_youtube.utils.cacheImage
 import com.ruangaldo.video_youtube.utils.visible
 
 class FirstFragment : BaseFragment<FragmentFirstBinding>() {
-    private var playScreenFragment = PlayerScreen.newInstance()
+    // Get one data dummy
+    private var _dataDummy = DataDummy.generateAudioArticleListDummy().first()
+
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentFirstBinding
         get() = FragmentFirstBinding::inflate
 
     override fun setup() {
+        binding.topImage.cacheImage(this._dataDummy.thumbnail)
+
         binding.button.setOnClickListener {
             binding.layoutTtsPlayed.visible()
 
+            val mBundle = Bundle()
+            mBundle.putParcelable(EXTRA_ARTICLE, _dataDummy)
+
+            val playerScreenFragment = PlayerScreen()
+            playerScreenFragment.arguments = mBundle
+
             childFragmentManager.beginTransaction()
-                .replace(R.id.play_screen_frame_layout, playScreenFragment)
+                .replace(R.id.play_screen_frame_layout, playerScreenFragment)
                 .commitAllowingStateLoss()
         }
         binding.button2.setOnClickListener {
