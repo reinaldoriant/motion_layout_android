948caede436e68eb4ae36cc07b5da575b82c1065
Alva Yonara Puramandya <alvayonara@outlook.com>
2021-04-09 09:47:08 +0700

    + add listener in another article

--- app/src/main/java/com/ruangaldo/video_youtube/ui/PlayerScreen.kt
+++ app/src/main/java/com/ruangaldo/video_youtube/ui/PlayerScreen.kt
@@ -3,10 +3,14 @@ package com.ruangaldo.video_youtube.ui
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.ruangaldo.video_youtube.R
 import com.ruangaldo.video_youtube.base.BaseFragment
 import com.ruangaldo.video_youtube.databinding.FragmentPlayerScreenBinding
 import com.ruangaldo.video_youtube.model.Article
 import com.ruangaldo.video_youtube.utils.cacheImage
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class PlayerScreen : BaseFragment<FragmentPlayerScreenBinding>() {
@@ -32,6 +36,17 @@ class PlayerScreen : BaseFragment<FragmentPlayerScreenBinding>() {
             binding.tvAnotherArticle.setOnClickListener {
                 startActivity(intent)
             }*/
+
+            binding.tvAnotherArticle.setOnClickListener {
+                // Call func changeToPlayList() from FirstPageFirstFragment to replace parent layout.
+                (parentFragment as FirstPageFirstFragment).changeToPlaylist()
+
+                // Call transition to start (minimize) programatically.
+                lifecycleScope.launch {
+                    delay(200)
+                    binding.layoutPlayerScreen.transitionToStart()
+                }
+            }
         }
     }
 }
