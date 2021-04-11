97677c74aba97c881d0f04ac779b79c928b19646
Alva Yonara Puramandya <alvayonara@outlook.com>
2021-04-11 14:34:20 +0700

    + add popBackStack() to exit PlayerScreen fragment

--- app/src/main/java/com/ruangaldo/video_youtube/ui/PlayerScreen.kt
+++ app/src/main/java/com/ruangaldo/video_youtube/ui/PlayerScreen.kt
@@ -1,7 +1,10 @@
 package com.ruangaldo.video_youtube.ui
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.lifecycle.lifecycleScope
 import com.ruangaldo.video_youtube.R
 import com.ruangaldo.video_youtube.base.BaseFragment
@@ -45,6 +48,35 @@ class PlayerScreen : BaseFragment<FragmentPlayerScreenBinding>() {
                     binding.layoutPlayerScreen.transitionToStart()
                 }
             }
+
+            /**
+             * Execute popBackStack() when swipe to dismiss PlayScreen fragment (drag down)
+             * References: https://proandroiddev.com/building-swipeview-using-motionlayout-7a80fd06401c
+             */
+            binding.layoutPlayerScreen.addTransitionListener(object :
+                MotionLayout.TransitionListener {
+                override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                }
+
+                override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                }
+
+                override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                    if (p1 == R.id.screen_close) {
+                        // Call popBackStack() to exit PlayerScreen fragment.
+                        parentFragmentManager.popBackStack()
+                        Toast.makeText(requireActivity(), "player screen closed.", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onTransitionTrigger(
+                    p0: MotionLayout?,
+                    p1: Int,
+                    p2: Boolean,
+                    p3: Float,
+                ) {
+                }
+            })
         }
     }
 
@@ -61,5 +93,13 @@ class PlayerScreen : BaseFragment<FragmentPlayerScreenBinding>() {
             delay(200)
             binding.layoutPlayerScreen.transitionToEnd()
         }
+    }
+
+    /**
+     * Used to check is PlayerScreen destroyed when exit this fragment using popBackStack().
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("check if destroyed", "true")
     }
 }
